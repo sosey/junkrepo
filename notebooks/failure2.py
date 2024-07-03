6d51ab350ed4a4b0b38dcc5d22ca60b05106b2cd
Megan Sosey <sosey@stsci.edu>
2024-07-03 19:18:47 -0400

    added more tests

---
+++ notebooks/failure2.py
@@ -0,0 +1,7 @@
+import os
+
+def test_bad_except():
+        # When the config file doesn't exist
+        cfg_file = os.path.join(os.getcwd(), 'notafile')
+        try:
+            print("no except clause")
